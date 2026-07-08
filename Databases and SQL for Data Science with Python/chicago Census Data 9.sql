18a8d55323b023c314a0b06297b679c30ea21b76
Richard Lam <81891771+richardlam4391@users.noreply.github.com>
2026-07-07 21:18:48 -0400

    Delete Databases and SQL for Data Science with Python/chicago Census Data 9.sql

--- Databases and SQL for Data Science with Python/chicago Census Data 9.sql
+++
@@ -1,5 +0,0 @@
--- Use a sub-query to find the name of the community area with highest hardship index.
-Select community_area_name, hardship_index from sample_csv.chicagocensusdata
-where hardship_index = (
-	Select max(hardship_index) from sample_csv.chicagocensusdata
-);
